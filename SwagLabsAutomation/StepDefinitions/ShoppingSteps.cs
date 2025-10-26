31ae52148a1a338acc12f38122406247a61a38fb
alexb <aleksbborisov@gmail.com>
2025-10-26 12:39:52 +0200

    asd

---
+++ SwagLabsAutomation/StepDefinitions/ShoppingSteps.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Playwright;
+using NUnit.Framework;
+using Reqnroll;
+using SwagLabsAutomation.PageObjects;
+
+namespace SwagLabsAutomation.StepDefinitions;
+
+[Binding]
+public class ShoppingSteps
+{
+    private readonly BrowserContext _browserContext;
+    private readonly LoginPage _loginpage;
+    private readonly ProductsPage _productsPage;
+
+    public ShoppingSteps(BrowserContext browserContext)
+    {
+        _browserContext = browserContext;
+        _loginpage = new LoginPage(_browserContext.Page!);
+        _productsPage = new ProductsPage(_browserContext.Page!);
+    }
+    
+    [Given("the user is on product catalog page")]
+    public async Task GivenTheUserIsOnProductCatalogPage()
+    {
+        await _loginpage.IsOnProductPage();
+    }
+    
+    [When("the user adds {string} to cart")]
+    public async Task WhenTheUserAddsToCart(string productName)
+    {
+        await _productsPage.AddProductToCart(productName);
+    }
+
+    [Then("the cart badge shows {string}")]
+    public async Task ThenTheCartBadgeShows(string count)
+    {
+        var actualCount = await _productsPage.GetCartBadgeCount();
+        Assert.That(actualCount, Is.EqualTo(count));
+    }
+
+    [Then("remove button is displayed for {string}")]
+    public async Task ThenRemoveButtonIsDisplayedFor(string productName)
+    {
+        await _productsPage.VerifyRemoveButtonVisible(productName);
+    }
+}
